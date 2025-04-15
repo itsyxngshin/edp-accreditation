fb1272186d115ac198a2f855b64ab63e8b8fbf77
Adornado B. Cabalbag Jr. <acabalbag22@gmail.com>
2025-04-15 23:34:46 +0800

    add lab files

---
+++ Accreditation/Accreditation/Form4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accreditation
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1(); // Create instance of Form2
+            form1.Show();              // Show Form2
+            this.Hide();
+        }
+    }
+}
